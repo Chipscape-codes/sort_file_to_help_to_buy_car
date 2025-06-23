06641ef48e230adb714d9022efe0414f3dbb3f48
Chipscape-codes <chipscape15@gmail.com>
2025-06-23 21:34:45 +0530

    Add files via upload

--- sort_to_help_buy_car.py
+++ sort_to_help_buy_car.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv("used_cars_data.csv")
-
 print(data.shape)
 print(data['Location'].head())
 city = input("Enter the city name: ").strip().lower()
@@ -11,15 +12,18 @@ data1 = data[data['Location'].str.lower() == city].reset_index(drop=True)
 
 c = float(input("Enter the car price max limit in lakhs: "))
 j = float(input("Enter the car price to find the minimum in lakhs: "))
+
 d = []
 f = []
 kilometers = []
+
 for i in range(len(data1)):
     if j < data1['Price'][i] < c:
         print(data1['Name'][i], ":", data1['Price'][i])
         d.append(data1['Name'][i])
         f.append(data1['Price'][i])
         kilometers.append(data1['Kilometers_Driven'][i])
+        
 dict = {'Name': d, 'Price': f, 'Kilometers_Driven': kilometers}
 df = pd.DataFrame(dict)
 print(df)
@@ -44,22 +48,48 @@ max_km_row = df.loc[df['Kilometers_Driven'].idxmax()]
 print("\nCar with highest kilometers driven:")
 print(max_km_row)
 
-#spec of the car with lowest price:
+
 min_price_spec = data1.loc[data1['Price'] == min_price_row['Price']]
 print("\nSpec of the car with lowest price:")
 print(min_price_spec)
 
-#spec of the car with lowest kilometers driven:
 min_km_spec = data1.loc[data1['Kilometers_Driven'] == min_km_row['Kilometers_Driven']]
 print("\nSpec of the car with lowest kilometers driven:")
 print(min_km_spec)
 
-# Price distribution
+
 sns.histplot(df['Price'], bins=50)
 plt.title("Price Distribution")
 plt.show()
 
-# Kilometers driven vs. Price
+
 sns.histplot(data=df, x=df['Kilometers_Driven'], y=df['Price'], hue=data1["Location"], bins=50, palette="colorblind")
 plt.title("Price vs Kilometers Driven")
+plt.show()
+
+x = df['Kilometers_Driven'].values.reshape(-1, 1)
+y = df['Price'].values.reshape(-1, 1)
+model1 = KNeighborsRegressor()
+model2 = LinearRegression()
+model1.fit(x, y)
+model2.fit(x, y)
+cost =int(input("Enter the kilometers driven to predict the price: "))
+print(model1.predict([[cost]]))  
+print(model2.predict([[cost]]))  
+
+
+
+
+plt.scatter(x, y, color='blue', label='Data Points')
+plt.scatter(cost, model1.predict([[cost]]), color='red', label='Predicted Price')
+plt.xlabel('Kilometers Driven')
+plt.ylabel('Price')
+plt.show()
+
+
+
+plt.scatter(x,y,color='blue', label='Data Points')
+plt.plot(x, model2.predict(x), color='black')
+plt.xlabel('Kilometers Driven')
+plt.ylabel('Price')
 plt.show()
